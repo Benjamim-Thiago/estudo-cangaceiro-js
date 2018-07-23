8baebe14d36cd17d8ad5f2ea691a083a52a40353
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2018-07-23 06:41:40 -0300

    criação da view de mensagem

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,12 +1,15 @@
 class NegociacaoController {
     constructor() {
-        let $ = document.querySelector.bind(document);
+        const $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes =  new Negociacoes();
         this._negociacoesView =  new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
+        this._mensagem =  new Mensagem();
+        this._mensagemView = new MensagemView('#MensagemView');
+        this._mensagemView.update(this._mensagem);
     }
     _limpaCampos() {
         this._inputData.value = '',
@@ -16,9 +19,11 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = "Negociação adicionada com sucesso!!!";
         this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update(this._mensagem);
         this._limpaCampos();
-        console.log(this._negociacoes.paraArray());
+        //console.log(this._negociacoes.paraArray());
     }
 
     _criaNegociacao() {
